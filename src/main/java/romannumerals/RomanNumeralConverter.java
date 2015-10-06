5c4aa4fafbfa58fd09024cd69bd2b6ff5ba51a59
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-06 14:55:05 +0100

    Converts 50 to L

--- src/main/java/romannumerals/RomanNumeralConverter.java
+++ src/main/java/romannumerals/RomanNumeralConverter.java
@@ -3,11 +3,13 @@ package romannumerals;
 public class RomanNumeralConverter {
     public String convert(int decimal) {
 
-        if(decimal == 1) {
+        if (decimal == 1) {
             return "I";
         } else if (decimal == 5) {
             return "V";
+        } else if (decimal == 10) {
+            return "X";
         }
-        return "X";
+        return "L";
     }
 }
--- src/test/java/romannumerals/RomanNumeralConverterTest.java
+++ src/test/java/romannumerals/RomanNumeralConverterTest.java
@@ -1,26 +1,36 @@
 package romannumerals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RomanNumeralConverterTest {
 
+    private RomanNumeralConverter converter;
+
+    @Before
+    public void setup() {
+        converter = new RomanNumeralConverter();
+    }
+
     @Test
     public void converts1ToI() {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
         assertEquals(converter.convert(1), "I");
     }
 
     @Test
     public void converts5ToV() {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
         assertEquals(converter.convert(5), "V");
     }
 
     @Test
     public void converts10ToX() {
-        RomanNumeralConverter converter = new RomanNumeralConverter();
         assertEquals(converter.convert(10), "X");
+    }
+
+    @Test
+    public void converts50ToL() {
+        assertEquals(converter.convert(50), "L");
     }
 }
