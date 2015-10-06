47a62149b0e18d0fcc5d6fc829f3a79b23bac556
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-06 14:59:36 +0100

    Generalises the single character cases into a map

--- src/main/java/romannumerals/RomanNumeralConverter.java
+++ src/main/java/romannumerals/RomanNumeralConverter.java
@@ -1,15 +1,20 @@
 package romannumerals;
 
-public class RomanNumeralConverter {
-    public String convert(int decimal) {
+import java.util.HashMap;
+import java.util.Map;
 
-        if (decimal == 1) {
-            return "I";
-        } else if (decimal == 5) {
-            return "V";
-        } else if (decimal == 10) {
-            return "X";
-        }
-        return "L";
+public class RomanNumeralConverter {
+    private Map<Integer, String> decimalToRomanNumeral;
+
+    public RomanNumeralConverter() {
+        decimalToRomanNumeral = new HashMap();
+        decimalToRomanNumeral.put(1, "I");
+        decimalToRomanNumeral.put(5, "V");
+        decimalToRomanNumeral.put(10, "X");
+        decimalToRomanNumeral.put(50, "L");
+    }
+
+    public String convert(int decimal) {
+        return decimalToRomanNumeral.get(decimal);
     }
 }
