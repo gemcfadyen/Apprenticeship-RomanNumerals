92fe89d477164fd74cb2c67f401aa1233ab604ed
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-07 13:32:51 +0100

    Tidies up the logic

--- src/main/java/romannumerals/RomanNumeralConverter.java
+++ src/main/java/romannumerals/RomanNumeralConverter.java
@@ -13,44 +13,25 @@ public class RomanNumeralConverter {
         initialiseDataForSubtractionRules();
     }
 
-    /**
-     * while !- 0
-     * do direct lookup
-     * do special cases
-     * do usual
-     */
-
-    private String findDirectConversionIfAvailable(int input) {
-        String romanNumeral = decimalToRomanNumeral.get(input);
-        if (noDirectTranslationFoundFor(romanNumeral)) {
-            romanNumeral = "";
-        }
-        return romanNumeral;
-    }
-
     public String convert(int input) {
         int currentDecimal = input;
-        String romanNumeral = decimalToRomanNumeral.get(currentDecimal);
+        String calculatedRomanNumeral = "";
 
-        if (noDirectTranslationFoundFor(romanNumeral)) {
-            String calculatedRomanNumeral = new String();
+        String romanNumeral = directConversion(currentDecimal);
+        if (!converted(romanNumeral)) {
 
             while (currentDecimal > 0) {
 
-                int currentDecimalInUnitForm = determineNumberAsItsUnit(currentDecimal);
-                System.out.println("Number in its unit is: " + currentDecimalInUnitForm);
-                String subtractionResult = applySubtractionRule(currentDecimalInUnitForm);
-                if (subtractionResult != "") {
-                    calculatedRomanNumeral = calculatedRomanNumeral + subtractionResult;
-                    currentDecimal = currentDecimal - currentDecimalInUnitForm;
-                    if (currentDecimal == 0) {
-                        return calculatedRomanNumeral;
-                    }
-                } else {
+                int decimalInUnitForm = unitFormOf(currentDecimal);
+                String convertedNumeral = applySubtractionRulesIfAppropriate(decimalInUnitForm);
 
-                    int derivedDecimal = findTheKeyLessThanOrEqualTo(currentDecimal);
-                    currentDecimal = currentDecimal - derivedDecimal;
-                    calculatedRomanNumeral = calculatedRomanNumeral + decimalToRomanNumeral.get(derivedDecimal);
+                if (!empty(convertedNumeral)) {
+                    currentDecimal = calculateRemainingDecimal(currentDecimal, decimalInUnitForm);
+                    calculatedRomanNumeral = append(calculatedRomanNumeral, convertedNumeral);
+                } else {
+                    int derivedDecimal = findNearestConversionLessThan(currentDecimal);
+                    currentDecimal = calculateRemainingDecimal(currentDecimal, derivedDecimal);
+                    calculatedRomanNumeral = append(calculatedRomanNumeral, decimalToRomanNumeral.get(derivedDecimal));
                 }
             }
 
@@ -60,9 +41,13 @@ public class RomanNumeralConverter {
         return romanNumeral;
     }
 
+    private int calculateRemainingDecimal(int currentDecimal, int decimalInUnitForm) {
+        currentDecimal = currentDecimal - decimalInUnitForm;
+        return currentDecimal;
+    }
 
     private void initialiseStandardConversions() {
-        decimalToRomanNumeral = new HashMap();
+        decimalToRomanNumeral = new HashMap<>();
         decimalToRomanNumeral.put(1, "I");
         decimalToRomanNumeral.put(5, "V");
         decimalToRomanNumeral.put(10, "X");
@@ -73,57 +58,55 @@ public class RomanNumeralConverter {
     }
 
     private void initialiseDataForSubtractionRules() {
-        subtractionRules = new HashMap();
+        subtractionRules = new HashMap<>();
         subtractionRules.put(1, asList(5, 10));
         subtractionRules.put(10, asList(50, 100));
         subtractionRules.put(100, asList(500, 1000));
     }
-    
-    private String applySubtractionRule(int inputInUnitForm) {
+
+    private String directConversion(int currentDecimal) {
+        return decimalToRomanNumeral.get(currentDecimal);
+    }
+
+    private String applySubtractionRulesIfAppropriate(int inputInUnitForm) {
 
         for (Map.Entry<Integer, List<Integer>> subtractionEntry : subtractionRules.entrySet()) {
             List<Integer> validSubtractionEntries = subtractionEntry.getValue();
             for (Integer valid : validSubtractionEntries) {
                 if (valid - subtractionEntry.getKey() == inputInUnitForm) {
-                    System.out.println("Subtraction rule applied " + decimalToRomanNumeral.get(subtractionEntry.getKey()) + decimalToRomanNumeral.get(valid));
-                    return decimalToRomanNumeral.get(subtractionEntry.getKey()) + decimalToRomanNumeral.get(valid);
+                    return directConversion(subtractionEntry.getKey()) + directConversion(valid);
                 }
             }
         }
         return "";
-
     }
 
-    private int determineNumberAsItsUnit(int currentDecimal) {
-        Integer input = Integer.valueOf(currentDecimal);
-
-        String representationOfInput = String.valueOf(input);
-
+    private int unitFormOf(int currentDecimal) {
+        String representationOfInput = String.valueOf(currentDecimal);
         int lengthOfInput = representationOfInput.length();
-        if (lengthOfInput == 2) {
-            System.out.println("Tens...");
-            return 10 * Integer.valueOf(String.valueOf(representationOfInput.charAt(0)));
-        } else if (lengthOfInput == 3) {
-            System.out.println("Hundreds...");
-            return 100 * Integer.valueOf(String.valueOf(representationOfInput.charAt(0)));
-        } else if (lengthOfInput == 4) {
-            System.out.println("Thousands...");
-            return 1000 * Integer.valueOf(String.valueOf(representationOfInput.charAt(0)));
-        } else {
-            System.out.println("Not Applicable for rounding to unit...");
-            return currentDecimal;
-        }
+        return getFirstNumber(representationOfInput) * (int)Math.pow(10, lengthOfInput - 1);
     }
 
-
-    private boolean noDirectTranslationFoundFor(String romanNumeral) {
-        return romanNumeral == null;
+    private Integer getFirstNumber(String representationOfInput) {
+        return Integer.valueOf(String.valueOf(representationOfInput.charAt(0)));
     }
 
-    private int findTheKeyLessThanOrEqualTo(int decimal) {
+    private boolean converted(String romanNumeral) {
+        return romanNumeral != null;
+    }
+
+    private boolean empty(String input) {
+        return input == "";
+    }
+
+    private String append(String numeral, String appender) {
+        return numeral + appender;
+    }
+
+    private int findNearestConversionLessThan(int decimal) {
         int currentKeyLessThanDecimal = 1;
         for (int key : decimalToRomanNumeral.keySet()) {
-            if (key >= currentKeyLessThanDecimal && key <= decimal) {
+            if (key > currentKeyLessThanDecimal && key <= decimal) {
                 currentKeyLessThanDecimal = key;
             }
         }
