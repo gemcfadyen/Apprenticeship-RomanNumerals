66319cb8bed498deb00a1a8c14d68f43aff8496b
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-11-07 10:33:02 +0000

    Reorders the if statements and introduces else

--- src/main/java/romannumeralsarray/RomanNumeralConverter.java
+++ src/main/java/romannumeralsarray/RomanNumeralConverter.java
@@ -2,13 +2,12 @@ package romannumeralsarray;
 
 public class RomanNumeralConverter {
     public static String converts(int decimal) {
-        if(decimal == 4) {
-            return "IV";
-        }
-        if(decimal >= 5) {
+
+        if (decimal >= 5) {
             return "V";
-        }
-        if(decimal >= 1) {
+        } else if (decimal == 4) {
+            return "IV";
+        } else if (decimal >= 1) {
             return "I";
         }
         return "";
