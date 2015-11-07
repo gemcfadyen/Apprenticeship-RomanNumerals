83a73dc60ff3c84a8aa956eb0d0137f30fabb244
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-11-07 10:34:33 +0000

    Introduces array for first numeral

--- src/main/java/romannumeralsarray/RomanNumeralConverter.java
+++ src/main/java/romannumeralsarray/RomanNumeralConverter.java
@@ -3,9 +3,10 @@ package romannumeralsarray;
 public class RomanNumeralConverter {
     public static String converts(int decimal) {
 
+        String[] numerals = new String[] {"V"};
         String romanNumeral = "";
         if (decimal >= 5) {
-            romanNumeral = "V";
+            romanNumeral = numerals[0];
         } else if (decimal == 4) {
             romanNumeral = "IV";
         } else if (decimal >= 1) {
