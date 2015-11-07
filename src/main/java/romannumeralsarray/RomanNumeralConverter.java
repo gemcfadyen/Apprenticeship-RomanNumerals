c49f4706d5c91c0643f583d0201b2a62106f6ffd
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-11-07 10:36:34 +0000

    Uses arrays for the next decimal/numeral pair

--- src/main/java/romannumeralsarray/RomanNumeralConverter.java
+++ src/main/java/romannumeralsarray/RomanNumeralConverter.java
@@ -2,15 +2,15 @@ package romannumeralsarray;
 
 public class RomanNumeralConverter {
     public static String converts(int decimal) {
-        String[] numerals = new String[] {"V"};
-        int[] decimals = new int[] {5};
+        String[] numerals = new String[] {"V", "IV"};
+        int[] decimals = new int[] {5, 4};
 
         String romanNumeral = "";
 
         if (decimal >= decimals[0]) {
             romanNumeral = numerals[0];
-        } else if (decimal == 4) {
-            romanNumeral = "IV";
+        } else if (decimal == decimals[1]) {
+            romanNumeral = numerals[1];
         } else if (decimal >= 1) {
             romanNumeral = "I";
         }
